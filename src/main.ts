06e7e8fee83de6dcc331ea7500882ac40b7d7902
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 10:26:18 +0600

    Add js to  export path

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-export { generateHelpOfCliAppsInMarkdownFormat, type GenerateHelpOfCliAppsInMarkdownFormatOptions } from "./generate-help-of-cli-apps-in-markdown-format";
+export { generateHelpOfCliAppsInMarkdownFormat, type GenerateHelpOfCliAppsInMarkdownFormatOptions } from "./generate-help-of-cli-apps-in-markdown-format.js";
