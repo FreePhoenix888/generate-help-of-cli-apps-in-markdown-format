6692e7764a05af5bc3077f5c8d41a884d2ffe4d9
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-30 13:49:38 +0600

    Add output option

--- src/generate-help-of-cli-apps-in-markdown-format.ts
+++ src/generate-help-of-cli-apps-in-markdown-format.ts
@@ -1,5 +1,42 @@
 import path from 'path';
 import {execa} from 'execa';
+import fsExtra from 'fs-extra'
+
+export interface BaseOutputOptions {
+  /**
+   * Path of the file to write the table of contents to
+   */
+  filePath: string;
+}
+
+export interface WriteModeOptions {
+  /**
+   * Write mode for the output
+   */
+  writeMode: 'overwrite' | 'append' | 'replace-placeholder';
+}
+
+export interface OutputOptionsWithoutPlaceholder extends BaseOutputOptions, WriteModeOptions {
+  writeMode: 'overwrite' | 'append';
+}
+
+export interface Placeholder {
+  /**
+   * Start of the placeholder text
+   */
+  start: string;
+  /**
+   * End of the placeholder text
+   */
+  end: string;
+}
+
+export interface OutputOptionsWithPlaceholder extends BaseOutputOptions, WriteModeOptions {
+  writeMode: 'replace-placeholder';
+  placeholder: Placeholder;
+}
+
+export type OutputOptions = OutputOptionsWithoutPlaceholder | OutputOptionsWithPlaceholder;
 
 export interface GenerateHelpOfCliAppsInMarkdownFormatOptions {
   /**
@@ -13,6 +50,10 @@ export interface GenerateHelpOfCliAppsInMarkdownFormatOptions {
    * 2
    */
   rootHeaderLevel?: number;
+  /**
+   * Output options
+   */
+  output?: OutputOptions;
 }
 
 /**
@@ -44,6 +85,25 @@ ${`#`.repeat(rootHeaderLevel+1)} \`${cliUtilityFileNameWithoutExtension}\`
 ${helpMessage.replace(cliUtilityFileNameWithExtension, cliUtilityFileNameWithoutExtension)}
 \`\`\`
 `;
+  }
+
+  if(options.output) {
+    if(options.output.writeMode === 'replace-placeholder') {
+      const placeholderStart = options.output.placeholder.start;
+      const placeholderEnd = options.output.placeholder.end;
+      const placeholderRegex = new RegExp(`${placeholderStart}[\S\s]*${placeholderEnd}`, 'g');
+      const filePath = options.output.filePath;
+      const markdown = fsExtra.readFileSync(filePath, 'utf-8');
+      const newFileContents = markdown.replace(placeholderRegex, `${placeholderStart}\n${allHelpMessages}\n${placeholderEnd}`);
+      fsExtra.writeFileSync(filePath, newFileContents)
+    } else  {
+      const fileContents = fsExtra.readFileSync(options.output.filePath, 'utf-8');
+      if(options.output.writeMode === 'append') {
+        fsExtra.appendFileSync(options.output.filePath, `${fileContents}\n${allHelpMessages}`)
+      } else if(options.output.writeMode === 'overwrite') {
+        fsExtra.writeFileSync(options.output.filePath, allHelpMessages)
+      }
+    }
   }
 
   return allHelpMessages
